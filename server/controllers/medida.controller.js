f5ce75876690241124162b478847f1cee0392685
Gonzalo Garcia <ggarciah@eafit.edu.co>
2020-02-09 19:17:12 -0500

    auth back listo

--- server/controllers/medida.controller.js
+++ server/controllers/medida.controller.js
@@ -1,5 +1,14 @@
-const Medida = require('../models/medida');
+/**
+ * Usuario para pruebas:
+ * "email": "user@adm.com",
+ * "password": "adm",
+ */
 
+const Medida = require('../models/medida');
+const User = require('../models/user');
+
+const config = require('../config');
+const jwt = require('jsonwebtoken');
 const medidaCtrl = {};
 
 medidaCtrl.getMedida = async (req, res) => {
@@ -23,6 +32,24 @@ medidaCtrl.editMedida = async (req, res) => {
 }
 
 medidaCtrl.createMedida = async (req, res) => {
+
+    // Validando usuario    
+    const { email, password } = req.body;
+    const user = await User.findOne({email: email});
+    if(!user){
+        return res.status(404).send("The email doesn't exists");
+    }
+
+    const validPassword = await user.validatePassword(password);
+    if(!validPassword){
+        return res.status(401).json({auth: false, token: null});
+    }
+
+    const token = jwt.sign({id: user._id}, config.secret, {
+        expiresIn: 60 * 60 * 24
+    });
+
+    // Creando medida
     const medida = new Medida({
         nameSensor: req.body.nameSensor,
         temp: req.body.temp,
--- server/models/medida.js
+++ server/models/medida.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const MedidaSchema = new Schema({
+    email: { type: String, required: false },
+    password: { type: String, required: false },
     nameSensor: { type: String, required: true },
     temp: { type: Number, required: true },
     hum: { type: Number, required: true },
